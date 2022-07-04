c8bccb99d87c5ba795bbd9963128e3ed286c3c09
nalowageena <nalowageena654@gmail.com>
2022-07-04 11:47:11 +0100

    created sign in and sign up pages

---
+++ app.js
@@ -0,0 +1,7 @@
+// const splash = document.querySelector(".splash-screen");
+
+// document.addEventListener("DOMContentLoaded", (e)=>{
+//     setTimeout(() => {
+//         splash.classList.add('display-none')
+//     }, 2000);
+// });
