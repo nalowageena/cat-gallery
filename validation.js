f5eded4d9adb85610c1778bf8df91390737240ac
Nalowa Geena <77391048+nalowageena@users.noreply.github.com>
2022-07-20 16:23:48 +0100

    toggle error warning when input is empty

--- validation.js
+++ validation.js
@@ -1,10 +1,12 @@
+// writing regex expressions for the 
+
 let email_regex = /^[A-Za-z-.]+\w*@[A-Za-z]+.[a-z]{2,3}$/;
 
 let my_email = 'somethi.n-g@gmail.com';
 
 console.log(email_regex.test(my_email));
 
-let username_regex = /^[A-Za-z]+[A-Za-z0-9-.@]*$/;
+let username_regex = /^[A-Za-z]+.{3,}$/;
 
 let my_name = 'Nalowa_geena';
 
@@ -16,9 +18,30 @@ console.log(username_regex.test(my_name));
 // it should have a capital letter
 // it should have a number
 // it should have small case letters
-let special_char = "~`!@#\$%\^&\*()_-+={\\[}]|:;\"\'<,>.?/";
+
 let password_regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.'\@#-+=?/,+_()£"!$%^&*|])(?=.{8,})/;
 
 let my_password = 'myblog1';
 
 console.log(password_regex.test(my_password));
+
+// function tnat activates on click button sign in
+
+// first select dom elements
+
+const warn = document.querySelector(".warn");
+const username = document.getElementById('username');
+const password = document.getElementById('password')
+const btn = document.querySelector(".btn");
+// listen for an event
+document.addEventListener("click", checkValidation);
+
+btn.disabled = true;
+function checkValidation() {
+    if (username.innerHTML === '') {
+        warn.classList.toggle('show');
+    }
+    else{
+        btn.disabled = false;
+    }
+}
